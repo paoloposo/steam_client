fe284760a09cce8a2d83a938565842d029373011
Paolo Poso <paolo@paoloposo.de>
2021-01-24 16:50:31 +0100

    Initial commit

---
+++ example/steam_client_example.dart
@@ -0,0 +1,6 @@
+import 'package:steam_client/steam_client.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/steam_client.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library steam_client;
+
+export 'src/steam_client_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
