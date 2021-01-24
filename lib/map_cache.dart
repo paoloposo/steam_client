91197947be986f5cdd0f81d0b06d58bc19d29cb8
Paolo Poso <paolo@paoloposo.de>
2021-01-24 18:01:46 +0100

    Add documentation to map_cache library

--- lib/map_cache.dart
+++ lib/map_cache.dart
@@ -1,4 +1,8 @@
 
+/// An implementation of a simple cache that stores data in a [Map] in memory.
+///
+/// It supports loading multiple items simultaneously to more efficiently use
+/// the underlying structure.
 library map_cache;
 
 export 'src/MapCache.dart';
--- lib/src/MapCache.dart
+++ lib/src/MapCache.dart
@@ -1,11 +1,33 @@
 
+/// Callback to load items that are not in the cache.
+///
+/// Gets a list of [keys] to load and returns a [Future] list of corresponding
+/// items. Items in the returned list must be in the same order as [keys].
+typedef Loader<S, T> = Future<List<T>> Function(List<S> keys);
+
+/// A simple cache that stores data in a [Map] in memory.
+///
+/// MapCache supports loading items that are not yet in the cache.
+/// Multiple items can be loaded simultaneously to more efficiently use the
+/// underlying structure.
 class MapCache<S, T> {
 
+  /// The cache [Map].
   Map<S, T> cache = {};
-  Future<List<T>> Function(List<S> keys) loader;
+
+  /// Callback to load items that are not in the cache.
+  final Loader<S, T> loader;
 
   MapCache(this.loader);
 
+  /// Get a single item by its [key].
+  ///
+  /// If the item is not yet in the cache, it will be loaded using the [loader]
+  /// and subsequently added to the cache.
+  /// If the item is already in the cache, setting [forceRefresh] to true will
+  /// re-load the item and replace its entry in the cache.
+  ///
+  /// Returns a [Future] that resolves to the item once loading is finished.
   Future<T> get(S key, {bool forceRefresh = false}) async {
 
     if (!cache.containsKey(key) || forceRefresh) {
@@ -15,6 +37,16 @@ class MapCache<S, T> {
     return cache[key];
   }
 
+  /// Get multiple items by their [keys].
+  ///
+  /// If the items are not yet in the cache, the will be loaded simultaneously
+  /// using the [loader].
+  /// If the items are already in the cache, setting [forceRefresh] to true will
+  /// force re-loading of all specified items and replace their entries in the
+  /// cache.
+  ///
+  /// Returns a [Future] that resolves to a list of items once loading is
+  /// finished.
   Future<List<T>> getMultiple(List<S> keys, {bool forceRefresh = false}) async {
 
     var items = <S, T>{};
@@ -37,16 +69,12 @@ class MapCache<S, T> {
     return keys.map((key) => items[key]).toList();
   }
 
+  /// Use the [loader] to load the items specified by their [keys].
   void _load(List<S> keys) async {
 
     var data = await loader(keys);
     for (var i=0; i<keys.length; i++) {
       cache[keys[i]] = data[i];
     }
-  }
-
-  void evict(S key) {
-
-    cache.remove(key);
   }
 }
